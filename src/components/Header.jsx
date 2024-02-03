7d0be7df0ac3d4ba13413b1e3181811d3b8e2ad4
denis-tsibaev <denis.tsibaev@gmail.com>
2024-02-03 14:42:12 +0700

    commit
    завис комп

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ReactLogo from '../images/cinema-logo.svg';
+import ReactLogo from '../assets/images/cinema-logo.svg';
 
 export const Header = () => {
     return (
--- src/pages/Seats.jsx
+++ src/pages/Seats.jsx
@@ -1,16 +1,66 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 export const Seats = () => {
-    const { hallName } = useParams();
-    console.log(hallName);
+    // const { hallName } = useParams();
+    // console.log(hallName);
+
+    // switch (hallName) {
+    //     case hallName === 'Red':
+    //         return <div>Red</div>;
+    //     case hallName === 'Green':
+    //         return <div>Green</div>;
+    //     case hallName === 'Blue':
+    //         return <div>Blue</div>;
+
+    //     default:
+    //         return <div>Default</div>;
+    // }
+
+    // const x = [1, 2, 3, 4, 5, 6];
+
+    const red = {
+        row: [1, 2, 3, 4, 5, 6],
+        seat: 6,
+    };
+
+    // for (let i = 0; i <= red.row; i++) {
+    //     for (let j = 0; j <= red.seat; j++) {
+    //         console.log(i, j);
+    //     }
+    // }
+
+    // const seatsMarkUp = arr => {
+    //     for (let index = 0; index < arr.length; index++) {
+    //         console.log(arr[index]);
+
+    //         return (
+    //             <label>
+    //                 {arr[index]}
+    //                 <input
+    //                     type="checkbox"
+    //                     name=""
+    //                     id=""
+    //                     className="input-checkbox-seat"
+    //                 />
+    //             </label>
+    //         );
+    //     }
+    // };
 
     return (
         <div>
             <h1>Выбор места</h1>
-            <label>
-                qwe
-                <input type="checkbox" name="" id="" />
-            </label>
+            {red.row.map(el => (
+                <label key={el}>
+                    {el}
+                    <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        className="input-checkbox-seat"
+                    />
+                </label>
+            ))}
         </div>
     );
 };
