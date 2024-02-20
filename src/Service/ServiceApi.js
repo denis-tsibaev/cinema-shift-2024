589a430aa25328d4dcfa892c1c57fc39076c2660
denis-tsibaev <denis.tsibaev@gmail.com>
2024-02-20 23:12:26 +0700

    логин
    Начал делать логин пользователя

--- src/Service/ServiceApi.js
+++ src/Service/ServiceApi.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 export const BASE_URL = 'https://shift-backend.onrender.com';
+axios.defaults.baseURL = BASE_URL;
 
-export const getMoviesToday = () => axios.get(`${BASE_URL}/cinema/today`);
+export const getMoviesToday = () => axios.get('/cinema/today');
 
-export const getMovie = filmId =>
-    axios.get(`${BASE_URL}/cinema/film/${filmId}`);
+export const getMovie = filmId => axios.get(`/cinema/film/${filmId}`);
 
 export const getSchedule = filmId =>
-    axios.get(`${BASE_URL}/cinema/film/${filmId}/schedule`);
+    axios.get(`/cinema/film/${filmId}/schedule`);
+
+export const regNewUser = newUser =>
+    axios.post('users/signin', newUser, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { regNewUser } from '../service/serviceApi';
 
 export const Profile = () => {
+    const [newUserName, setNewUserName] = useState('');
+    const [newUserEmail, setNewUserEmail] = useState('');
+    const [newUserPhone, setNewUserPhone] = useState('');
+
+    const newUser = {
+        name: newUserName,
+        email: newUserEmail,
+        phone: newUserPhone,
+        // code: 345231,
+    };
+
+    const onNameChange = e => {
+        setNewUserName(e.target.value);
+    };
+    const onEmailChange = e => {
+        setNewUserEmail(e.target.value);
+    };
+    const onPhoneChange = e => {
+        setNewUserPhone(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        regNewUser(newUser);
+        console.log(newUser);
+        e.currentTarget.reset();
+    };
+
     return (
         <div>
             <h1>Личный кабинет</h1>
+            <h3>Зарегистрироваться</h3>
+            <form onSubmit={handleSubmit} className="form" autoComplete="off">
+                <label>
+                    Телефон
+                    <input
+                        type="phone"
+                        name="phone"
+                        required
+                        onChange={onPhoneChange}
+                    />
+                </label>
+
+                <label>
+                    Имя
+                    <input type="text" name="name" onChange={onNameChange} />
+                </label>
+                <label>
+                    Email
+                    <input type="email" name="email" onChange={onEmailChange} />
+                </label>
+                <button type="submit">Зарегистрироваться</button>
+            </form>
         </div>
     );
 };
